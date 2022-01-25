c0353af64a77a50dae2742b827040b5bb6c0868b
zanatabr <zanata@gmail.com>
2022-01-25 18:16:27 -0300

    14.6 - Panic e Recover

---
+++ 14 - Funcoes Avancadas/14.6 - Panic e Recover/panic-recover.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// regra: A média não pode ser 6
+// panic: para de executar o que está sendo feito, chama
+//   todas as funções que usam "defer". Se não recuperar a
+//   função, usando "recover", o programa morre
+func alunoEstaAprovado(n1, n2 float64) bool {
+	defer recuperarExecucao()
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+
+	panic("A MÉDIA É EXATAMENTE 6!")
+}
+
+func recuperarExecucao() {
+	if r := recover(); r != nil {
+		fmt.Println("Execução recuperada com sucesso!")
+	}
+}
+
+func main() {
+	fmt.Println(alunoEstaAprovado(8, 6))
+	fmt.Println("Pós execução!")
+}
