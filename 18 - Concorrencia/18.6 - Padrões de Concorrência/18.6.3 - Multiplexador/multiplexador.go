ef805219af4224b4a83f11e45aafa431bc9900ae
zanatabr <zanata@gmail.com>
2022-01-27 22:35:16 -0300

    18.6.3 - Multiplexador

---
+++ 18 - Concorrencia/18.6 - Padrões de Concorrência/18.6.3 - Multiplexador/multiplexador.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Multiplexador: juntar dois ou mais canais em apenas um canal
+
+func main() {
+
+	canal := multiplexar(escrever("Olá Mundo!"), escrever("Programando em Go!"))
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-canal)
+	}
+}
+
+func multiplexar(canalDeEntrada1, canalDeEntrada2 <-chan string) <-chan string {
+	canalDeSaida := make(chan string)
+
+	go func() {
+		for {
+			select {
+			case mensagem := <-canalDeEntrada1:
+				canalDeSaida <- mensagem
+			case mensagem := <-canalDeEntrada2:
+				canalDeSaida <- mensagem
+			}
+		}
+	}()
+
+	return canalDeSaida
+
+}
+
+func escrever(texto string) <-chan string {
+	canal := make(chan string)
+
+	go func() {
+		for {
+			canal <- fmt.Sprintf("Valor recebido: %s", texto)
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(2000)))
+		}
+	}()
+
+	return canal
+}
