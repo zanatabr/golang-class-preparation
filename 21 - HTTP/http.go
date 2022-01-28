3ee3542bbde6435607c1e366dbc8fafc2fffec87
zanatabr <zanata@gmail.com>
2022-01-28 18:46:41 -0300

    21 - HTTP

---
+++ 21 - HTTP/http.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// HTTP É UM PROTOCOLO DE COMUNICAÇÃO - BASE DA COMUNICAÇÃO WEB
+
+	// CLIENTE (FAZ REQUISIÇÃO) - SERVIDOR (PROCESSA REQUISIÇÃO E ENVIA RESPOSTA)
+
+	// Request - Response
+
+	// Rotas
+	// URI - Identificador do Recurso
+	// Método - GET, POST, PUT, DELETE
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // raiz
+		w.Write([]byte("Página Raiz!"))
+	})
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Olá Mundo!"))
+	})
+
+	http.HandleFunc("/usuarios", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Carregar página de usuários!"))
+	})
+
+	// Deixa um servidor rodando no port 5000
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
